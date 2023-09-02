3f70e42d16bc01266bf41e71fed2e9457c902183
manish <manishpanwar682@gmail.com>
2023-09-01 16:23:22 -1200

    first commit

---
+++ src/components/Container/Container.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import Form from "../Form/Form";
+import Table from "../Table/Table";
+
+import styles from "./Container.module.css";
+
+export default function Container() {
+  const [isLoading, setLoading] = useState(false);
+  const [errMsg, setErrMsg] = useState(null);
+  const [fetchedData, setFetchedData] = useState(null);
+
+  return (
+    <div className={styles.container}>
+      <Form
+        setLoading={setLoading}
+        setErrMsg={setErrMsg}
+        setFetchedData={setFetchedData}
+      />
+
+      {isLoading && (
+        // <img className={styles.spinner} src="/preloader.gif" alt="Loading..." />
+        <p>Loading...</p>
+      )}
+
+      {errMsg && <p className={styles.err}>{errMsg}</p>}
+
+      {!isLoading && !errMsg && fetchedData && (
+        <Table full={fetchedData.full} short={fetchedData.short} />
+      )}
+    </div>
+  );
+}
